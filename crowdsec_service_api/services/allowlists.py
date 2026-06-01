dc4b8c7328412de508d9eb31b555a8ccfb9ed86d
mazzma12 <23187108+mazzma12@users.noreply.github.com>
2026-06-01 09:58:20 +0000

    Update python SDK v0.15.34

--- crowdsec_service_api/services/allowlists.py
+++ crowdsec_service_api/services/allowlists.py
@@ -11,7 +11,7 @@ from ..http_client import HttpClient
 
 class Allowlists(Service):
     def __init__(self, auth: Auth, base_url: str = "https://admin.api.crowdsec.net/v1") -> None:
-        super().__init__(base_url=base_url, auth=auth, user_agent="crowdsec_service_api/v0.15.26")
+        super().__init__(base_url=base_url, auth=auth, user_agent="crowdsec_service_api/v0.15.34")
     
     def list_allowlists(
         self,
